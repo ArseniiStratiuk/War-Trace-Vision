5a7809caabd6770f3cc1ab810057fa474d148460
dhvgop-i <ivan.maksymchuk4@gmail.com>
2025-04-27 14:00:50 +0300

    allowed host

--- wartrace/wartrace/settings.py
+++ wartrace/wartrace/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%ronxm3e#_3$6e4xrqkpe*g=yp%pbx%b506ohh05mr33&98q2n
 DEBUG = True
 
 ALLOWED_HOSTS = ['10.10.224.181',
-                 '127.0.0.1',
+                 '127.0.0.1', 'war-trace-vision.onrender.com'
 ]
 
 
